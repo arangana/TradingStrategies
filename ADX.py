3565dea9e84044a41fc260748c532f3259ea09dd
arangana <arangana@andrew.cmu.edu>
2017-06-09 16:00:00 -0700

    latest changes

--- ADX.py
+++ ADX.py
@@ -6,16 +6,22 @@ import numpy as np
 ''' This system uses Average Directional Index (ADX) to allocate capital across desired equities. '''
 def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL, exposure, equity, settings):
 
+    '''
+    ADX Period - 14 is the most commonly used.
+    NOTE - Please set the correct value for ADX_period in the settings funcion.
+    '''
+    ADX_period = settings['ADX_period'] # OPTIMIZE HERE
+
     settings['TradeDay'] = settings['TradeDay'] + 1
     print 'Executing trade for day ', settings['TradeDay'], ' on trade date ', DATE[settings['lookback']-1]
 
     # Compute ADX for each market
     num_markets = settings['num_markets']
     for market in range(num_markets):
-        calculate_ADX(market, HIGH[:,market:market+1], LOW[:,market:market+1], CLOSE[:,market:market+1], settings)
+        calculate_ADX(market, HIGH[:,market:market+1], LOW[:,market:market+1], CLOSE[:,market:market+1], settings, ADX_period)
 
     # Execute trades based on trading strategy
-    weights = execute_trade(settings['ADX'], CLOSE)
+    weights = execute_trade(settings['ADX'], CLOSE, ADX_period)
 
     return weights, settings
 
@@ -25,10 +31,14 @@ def mySettings():
 
     settings= {}
 
-    settings['beginInSample'] = '20160321'
-    settings['endInSample'] = '20170320'
-    settings['markets']  = ['AAPL', 'BRKB', 'F_NG', 'F_GC']
-    settings['lookback'] = 28
+    #settings['beginInSample'] = '20160321'
+    #settings['endInSample'] = '20170320'
+    settings['markets']  = ['CASH','F_AD', 'F_BO', 'F_BP', 'F_C', 'F_CC', 'F_CD',
+    'F_CL', 'F_CT', 'F_DX', 'F_EC', 'F_ED', 'F_ES', 'F_FC','F_FV', 'F_GC',
+    'F_HG', 'F_HO', 'F_JY', 'F_KC', 'F_LB', 'F_LC', 'F_LN', 'F_MD', 'F_MP',
+    'F_NG', 'F_NQ', 'F_NR', 'F_O', 'F_OJ', 'F_PA', 'F_PL', 'F_RB', 'F_RU',
+    'F_S','F_SB', 'F_SF', 'F_SI', 'F_SM', 'F_TU', 'F_TY', 'F_US','F_W', 'F_XX',
+    'F_YM']
     settings['budget'] = 10**6
     settings['slippage'] = 0.05    
 
@@ -45,6 +55,10 @@ def mySettings():
     # ADX values for each of the selected markets ordered by index
     settings['ADX'] = [np.nan] * settings['num_markets']
 
+    # Set Period here
+    settings['ADX_period'] = 14
+    settings['lookback'] = settings['ADX_period'] * 2
+
     return settings
 
 
@@ -54,7 +68,7 @@ know that there is a strong trend. We then look at the average price
 over the last 14 trading days and check if there is an upward or 
 downward trend. Based on this we take on either a long or short position accordingly.
 '''
-def execute_trade(market_ADXs, CLOSE):
+def execute_trade(market_ADXs, CLOSE, ADX_period):
     CURRENT_DAY = CLOSE.shape[0] - 1
     num_markets = len(market_ADXs)
     ADX_sum = np.nansum(market_ADXs)
@@ -62,7 +76,9 @@ def execute_trade(market_ADXs, CLOSE):
     prop = [0.0] * num_markets
 
     # Prior 14 day average of closing prices
-    avg = np.nansum(CLOSE[13:27,:],axis=0) / 14.0
+    end = (ADX_period * 2) - 1
+    start = end - ADX_period
+    avg = np.nansum(CLOSE[start:end,:],axis=0) / (1.0 * ADX_period)
 
     for market in range(num_markets):
         # There is a strong trend
@@ -83,7 +99,7 @@ def execute_trade(market_ADXs, CLOSE):
 
 
 # Details of ADX calculations for a given market
-def calculate_ADX(market, HIGH, LOW, CLOSE, settings):
+def calculate_ADX(market, HIGH, LOW, CLOSE, settings, ADX_period):
     # Components required to calculate ADX - True Range, Directional Movements and Directional Indices
     tradeDayVals = {'TR':0.0, '+DM':0.0, '-DM':0.0, '+DI':0.0, '-DI':0.0, 'DX':0.0}
 
@@ -93,7 +109,7 @@ def calculate_ADX(market, HIGH, LOW, CLOSE, settings):
         
     # First calculation of ADX - i.e. no previous values of ADX available
     elif(np.isnan(settings['ADX'][market])):
-        for trade_day in range(1, 15):
+        for trade_day in range(1, ADX_period+1):
             # Add up / accumulate true range and directional movement values for first 14 days         
             settings['TR14'][market] = settings['TR14'][market] + calculate_TR(CLOSE[trade_day-1][0], HIGH[trade_day][0], LOW[trade_day][0])
             settings['+DM14'][market] = settings['+DM14'][market] + calculate_DM_plus(HIGH[trade_day][0], LOW[trade_day][0], HIGH[trade_day-1][0], LOW[trade_day-1][0])
@@ -104,16 +120,16 @@ def calculate_ADX(market, HIGH, LOW, CLOSE, settings):
         tradeDayVals['-DI'] = 100.0 * settings['-DM14'][market] / settings['TR14'][market]
         settings['ADX'][market] = calculate_DX(tradeDayVals['+DI'], tradeDayVals['-DI'])
 
-        for trade_day in range(15, 28):
+        for trade_day in range(ADX_period+1, ADX_period*2):
             # True range and directional movements for the current trade day
             tradeDayVals['TR'] = calculate_TR(CLOSE[trade_day-1][0], HIGH[trade_day][0], LOW[trade_day][0])
             tradeDayVals['+DM'] = calculate_DM_plus(HIGH[trade_day][0], LOW[trade_day][0], HIGH[trade_day-1][0], LOW[trade_day-1][0])
             tradeDayVals['-DM'] = calculate_DM_minus(HIGH[trade_day][0], LOW[trade_day][0], HIGH[trade_day-1][0], LOW[trade_day-1][0])
 
             # Wilder smoothing techniques to calculate 14 day smoothed true range and directional movements
-            settings['TR14'][market] = wilder_smooth(settings['TR14'][market], tradeDayVals['TR'])
-            settings['+DM14'][market] = wilder_smooth(settings['+DM14'][market], tradeDayVals['+DM'])
-            settings['-DM14'][market] = wilder_smooth(settings['-DM14'][market], tradeDayVals['-DM'])
+            settings['TR14'][market] = wilder_smooth(settings['TR14'][market], tradeDayVals['TR'], ADX_period)
+            settings['+DM14'][market] = wilder_smooth(settings['+DM14'][market], tradeDayVals['+DM'], ADX_period)
+            settings['-DM14'][market] = wilder_smooth(settings['-DM14'][market], tradeDayVals['-DM'], ADX_period)
 
             # Directional indices for the current trade day
             tradeDayVals['+DI'] = 100.0 * settings['+DM14'][market] / settings['TR14'][market]
@@ -124,7 +140,7 @@ def calculate_ADX(market, HIGH, LOW, CLOSE, settings):
             settings['ADX'][market] = settings['ADX'][market] + tradeDayVals['DX']
 
         # First calculation of ADX - average of first 14 DX values
-        settings['ADX'][market] = settings['ADX'][market] / 14.0
+        settings['ADX'][market] = settings['ADX'][market] / (1.0 * ADX_period)
 
     # Subsequent calculations of ADX
     else:
@@ -137,9 +153,9 @@ def calculate_ADX(market, HIGH, LOW, CLOSE, settings):
         tradeDayVals['-DM'] = calculate_DM_minus(HIGH[CURRENT_DAY][0], LOW[CURRENT_DAY][0], HIGH[PREVIOUS_DAY][0], LOW[PREVIOUS_DAY][0])
 
         # Wilder smoothing techniques to calculate 14 day smoothed true range and directional movements
-        settings['TR14'][market] = wilder_smooth(settings['TR14'][market], tradeDayVals['TR'])
-        settings['+DM14'][market] = wilder_smooth(settings['+DM14'][market], tradeDayVals['+DM'])
-        settings['-DM14'][market] = wilder_smooth(settings['-DM14'][market], tradeDayVals['-DM'])
+        settings['TR14'][market] = wilder_smooth(settings['TR14'][market], tradeDayVals['TR'], ADX_period)
+        settings['+DM14'][market] = wilder_smooth(settings['+DM14'][market], tradeDayVals['+DM'], ADX_period)
+        settings['-DM14'][market] = wilder_smooth(settings['-DM14'][market], tradeDayVals['-DM'], ADX_period)
 
         # Directional indices for the current trade day
         tradeDayVals['+DI'] = 100.0 * settings['+DM14'][market] / settings['TR14'][market]
@@ -147,13 +163,13 @@ def calculate_ADX(market, HIGH, LOW, CLOSE, settings):
         tradeDayVals['DX'] = calculate_DX(tradeDayVals['+DI'], tradeDayVals['-DI'])
 
         # Final ADX value for current trade day
-        settings['ADX'][market] = ((settings['ADX'][market] * 13.0) + (tradeDayVals['DX'])) / 14.0
+        settings['ADX'][market] = ((settings['ADX'][market] * 1.0 * (ADX_period-1)) + (tradeDayVals['DX'])) / (1.0 * ADX_period)
 
     return
 
 
-def wilder_smooth(prev_val, curr_val):
-    return prev_val - (prev_val / 14.0) + curr_val
+def wilder_smooth(prev_val, curr_val, ADX_period):
+    return prev_val - (prev_val / (1.0 * ADX_period)) + curr_val
 
 
 def calculate_TR(prevClose, currHigh, currLow):
